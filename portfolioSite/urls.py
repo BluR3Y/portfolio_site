78cc2956ca7ef3404607a2821a6ee1483c135c57
BluR3Y <69604218+BluR3Y@users.noreply.github.com>
2022-01-12 14:26:59 -0500

    sqlite tables and projects

--- portfolioSite/urls.py
+++ portfolioSite/urls.py
@@ -1,8 +1,14 @@
-
+from django.contrib import admin
 from django.urls import path
 from quote_generator import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', views.index, name='index'),
-    path('about/', views.about, name='about'),
+    # path('about/', views.about, name='about'),
+    # path('blog/', include('blog.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
